ec7084f184955efbde097e17697e2f61411d7a17
TheOphige <igetheophilus02@gmail.com>
2024-09-03 14:21:30 +0100

    gui looking good

--- app/book_type_extract/textbook_extract.py
+++ app/book_type_extract/textbook_extract.py
@@ -1,26 +1,3 @@
 def textbook_extract(uploaded_file):
     book =""
     return book
-
-
-
-# import streamlit as st
-
-# def extract_chapter():
-#     # Define the chapters dictionary
-#     summarized_books = {
-#         '1 intro': 'intro Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '2 fufu': 'fufu Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '3 eba': 'eba Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '4 lulaby': 'lulaby Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '5 gurima': 'gurima Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '6 gotnick': 'gotnick Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '7 fufus': 'fufus Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '8 ebas': 'ebas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '9 lulabys': 'lulabys Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '10 gurimas': 'gurimas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '11 gotnicks': 'gotnicks Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '12 gurimaqs': 'gurimas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...',
-#         '13 gotnickqs': 'gotnicks Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ...'
-#     }
-#     return summarized_books
